8674a9c119879db97efc473799fb6a2cbbe60cc9
Omar Sobhy <omar.elkord@gmail.com>
2024-12-05 02:46:59 +0200

    ui almost complete

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -6,6 +6,7 @@ import 'package:los_pollos_hermanos/models/customUser.dart';
 import 'package:los_pollos_hermanos/screens/Client/add_menu_item_screen.dart';
 import 'package:los_pollos_hermanos/screens/Client/choose_restaurant_screen.dart';
 import 'package:los_pollos_hermanos/screens/Client/main_page.dart';
+import 'package:los_pollos_hermanos/screens/Client/test_screen.dart';
 import 'package:los_pollos_hermanos/screens/Client/variations_screen.dart';
 import 'package:los_pollos_hermanos/screens/authenticate/authenticate.dart';
 import './Client/home.dart';
